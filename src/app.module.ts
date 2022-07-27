575ba9c70fc4681f470b16bb3ab475b6912baff9
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-07-27 10:23:08 -0500

    useValue :alien:

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,19 @@ import { ProductsService } from './products/services/products.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = '12323453';
+const API_KEY_PORD = 'PROD12746ys';
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService, ProductsService],
+  providers: [
+    AppService,
+    ProductsService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PORD : API_KEY, // $env:NODE_ENV = 'prod'; npm run start:dev
+    }
+  ],
 })
 export class AppModule {}
