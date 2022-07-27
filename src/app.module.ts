6f8479ba0d3764b353cd5f1f8716350481393646
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-07-27 16:35:53 -0500

    Configuración por ambientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,15 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
+import { enviroments } from './enviroments';
+
 const API_KEY = '12323453';
 const API_KEY_PORD = 'PROD12746ys';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
     }),
     HttpModule,
